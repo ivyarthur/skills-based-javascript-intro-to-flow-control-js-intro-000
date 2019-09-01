ba41a598a8b45bbc94844ebcdd5ba03e9f769c5a
Emma Scott <ejms236@gmail.com>
2019-09-01 13:01:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,3 +30,16 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 
 }
+
+/*
+switch (expression) {
+  case n:
+      // code to be executed if case n is true
+      break; // break out of switch statement once code executed
+  case m:
+      // code to be executed if case m is true
+      break; // break out of switch statement once code executed
+  default:  // all other cases
+      // code to be executed if case n and case m false
+}
+*/
